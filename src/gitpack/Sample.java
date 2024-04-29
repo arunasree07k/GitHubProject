6fc42a14c8f1538169f63effd103adf63ef161cc
lenovo <bhargavimadalam18@gmail.com>
2024-04-29 19:54:09 +0530

    modified file.........

--- src/gitpack/Sample.java
+++ src/gitpack/Sample.java
@@ -1,16 +1,16 @@
 package gitpack;
 
-class AdditionExample {
-	public int add(int x, int y) {
-		int sum=x+y;
+class DivisionExample {
+	public int div(int x, int y) {
+		int sum=x/y;
 		return sum;
 	}
 }
 
 public class Sample {
 	public static void main(String[] args) {
-		AdditionExample obj = new AdditionExample();
-		int result=obj.add(10, 20);
+		DivisionExample obj = new DivisionExample();
+		int result=obj.div(20, 10);
 		System.out.println("Sum of two numbers:"+result);
 	}
 }
